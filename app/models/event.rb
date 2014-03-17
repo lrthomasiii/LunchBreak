dacc2363b49bf550c505b814b417a75dbddb57ed
Lawrence R. Thomas III <lathomas@sas.upenn.edu>
2014-03-16 21:53:34 -0400

    homework 7

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,9 @@
 class Event < ActiveRecord::Base
   belongs_to :user
+  validates :title, :presence => true
+  dateFormatValidation = /\d{2}\/\d{2}\/\d{4}/
+  validates :date, format: {with: dateFormatValidation}
+  timeFormatValidation = /\d{2}:\d{2}/
+  validates :time, format: {with: timeFormatValidation}
+  validates :description, length: {maximum: 150}, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,10 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  	devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 	has_many :events, :dependent => :destroy
+
+
+	validates :password, confirmation: true
 end
